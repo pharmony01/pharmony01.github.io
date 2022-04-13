d4feaefa430b0d37ba8eaea4c2cf51511fddf28e
pharmony01 <rileypeters2016@gmail.com>
2022-04-13 14:35:23 -0400

    adding a canvas

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Oh this actually does work lmfao';
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0, 0, 80, 80);
